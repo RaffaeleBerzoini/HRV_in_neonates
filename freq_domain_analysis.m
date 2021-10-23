9793b14df5801efa4b7f28c2de8803e1111d4276
Aurora <aucolombo.ac@gmail.com>
2021-10-23 16:53:29 +0200

    Well ordered main

--- freq_domain_analysis.m
+++ freq_domain_analysis.m
@@ -1,4 +1,4 @@
-function [LF2HF_welch, LF2HF_YW]=freq_domain_analysis(RRintervals, r_peaks_pt, f, fs) % o r_peaks_fp
+function [LF2HF_welch, LF2HF_YW]=freq_domain_analysis(RRintervals, r_peaks_pt, f, fs, size, i, s) % o r_peaks_fp
 
 %% Pre-processing
 % removing the mean value
@@ -15,8 +15,8 @@ RRintervals_rs = interp1(r_peaks_pt(1:end-1)/fs, RRintervals, (r_peaks_pt/fs:1/f
 
 [h,pValue] = adftest(RRintervals_rs);
 if h==1
-    fprintf('The signal is stochastic! \n')
-
+    fprintf('\nThe signal is stochastic. \n\n')
+end
 % valutare se mettere una if o un errore
 %% Power spectrum density
 % Non-Parametric PSD
@@ -26,7 +26,7 @@ nfft = 1024;
 [PSD_welch,f] = pwelch(RRintervals_rs, hamming(window), overlap, nfft, f_rs);
 
 % Parametric PSD
-AR_order = 8;
+AR_order = 18;
 [PSD_YW,f] = pyulear(RRintervals_rs, AR_order, nfft, f_rs);
 
 %% Power indices
@@ -49,21 +49,21 @@ HF_YW = trapz(PSD_YW(f_HF));
 LF2HF_welch = LF_welch/HF_welch;
 LF2HF_YW = LF_YW/HF_YW;
 
-fprintf('Welch --> Low frequency power spectrum density: %f; High frequency power spectrum density: %f \n', LF_welch, HF_welch)
-fprintf('YW --> Low frequency power spectrum density: %f; High frequency power spectrum density: %f', LF_YW, HF_YW)
+fprintf('Welch Analysis:\n Low frequency power spectrum density: \t %f;\n High frequency power spectrum density: \t %f \n', LF_welch, HF_welch)
+fprintf('YW Analysis:\n Low frequency power spectrum density: \t %f; \n High frequency power spectrum density: \t %f \n', LF_YW, HF_YW)
 
 %% plots
 
-figure;
-subplot(1,3,1); plot(r_peaks_pt(1:end-1)/fs,RRintervals);
-title('RR_ECG','Interpreter','none'); xlabel('Time [s]'); ylabel('Duration [ms]'); ylim([-150 150]);
+figure(8);
+%subplot(1,3,1); plot(r_peaks_pt(1:end-1)/fs,RRintervals);
+%title('RR_ECG','Interpreter','none'); xlabel('Time [s]'); ylabel('Duration [ms]'); ylim([-150 150]);
 
-subplot(1,3,2); plot(f,PSD_welch);
-title('PSD_Welch RR_ECG','Interpreter','none'); xlabel('Frequency [Hz]'); ylabel('PSD Welch Method [ms^2/Hz]');
+subplot(2,size,i); plot(f,PSD_welch);
+title(strcat(s,' - PSD_Welch RR_ECG'),'Interpreter','none'); xlabel('Frequency [Hz]'); ylabel('PSD Welch Method [ms^2/Hz]');
 str = ['LF/HF=',num2str(LF2HF_welch)]; text(0.6,0.8*max(PSD_welch),str,'HorizontalAlignment','left');
 
-subplot(1,3,3); plot(f,PSD_YW);
-title('PSD_YW RR_ECG','Interpreter','none'); xlabel('Frequency [Hz]'); ylabel('PSD Yule-Walker Method [ms^2/Hz]');
+subplot(2,size,i+size); plot(f,PSD_YW);
+title(strcat(s,' - PSD_YW RR_ECG'),'Interpreter','none'); xlabel('Frequency [Hz]'); ylabel('PSD Yule-Walker Method [ms^2/Hz]');
 str = ['LF/HF=',num2str(LF2HF_YW)]; text(0.6,0.8*max(PSD_YW),str,'HorizontalAlignment','left');
 
 
--- main.m
+++ main.m
@@ -1,76 +1,91 @@
 %% Clear workspace
 clear; clc; close all;
 %% load subject data
-subject_number = 4; %Insert a number from 1 to 5
+subject_number = 2; % selection of the patient (from 1 to 5)
+
 min_height = [4000, 3900, 6200, 1000, 740]; % valori soglia per ognuno dei pazienti
 f_s = 500;
 
 [ecg, active_quiet_state] = getEcg_SleepActivity(subject_number);
 state_ecg = get_state_ecg(ecg, active_quiet_state, f_s); 
-
-
 t0 = 0; %estrarre t0 da state_ecg row2
-T = (length(ecg)/f_s)-(1/f_s); %estrarre T da state_ecg row2
-t = t0:1/f_s:T; 
-%% spectrum
-spectrum = abs(fft(ecg - mean(ecg) ,4096));
+for i=1:size(state_ecg,2)
+    s = state_ecg{1,i};
+    ecg = state_ecg{3,i};
+    T = state_ecg{2, i}(2)-state_ecg{2, i}(1); %estrarre T da state_ecg row2
+    t = t0:1/f_s:T; 
+    
+    %% Plot of ECG
+    
+    figure(1);
+    subplot(size(state_ecg,2),1,i); plot(t,ecg); title(strcat(s,' -',' ECG'), 'Interpreter', 'none'); xlabel('Time [s]'); ylabel('Amplitude [mV]');
+    
+    %% spectrum
 
-f = linspace(0,f_s,length(spectrum));
-w = f/f_s; %omega
+    spectrum = abs(fft(ecg - mean(ecg),4096));
 
-figure(1);
-subplot(1,2,1); plot(t,ecg); title('ECG', 'Interpreter', 'none'); xlabel('Time [s]'); ylabel('Amplitude [mV]');
-subplot(1,2,2); plot(f,spectrum); title('Spectrum', 'Interpreter', 'none'); xlabel('Frequency [Hz]'); ylabel('|X(f)|');
-
+    f = linspace(0,f_s,length(spectrum));
+    w = f/f_s; %omega
+    
 %% filters application
 
-filter_order = 3;
-fcut = 50;       % cut-off frequency [Hz]
-wc = fcut/(f_s/2); % NORMALIZED cut-off frequency
-[b,a] = butter(filter_order,wc,'low');
-ecg=filtfilt(b, a, ecg);
-figure(2); freqz(b,a,1024,f_s);
+    filter_order = 3;
+    fcut = 50;       % cut-off frequency [Hz]
+    wc = fcut/(f_s/2); % NORMALIZED cut-off frequency
+    [b,a] = butter(filter_order,wc,'low');
+    ecg=filtfilt(b, a, ecg);
+    
+    figure(2);
+    freqz(b,a,1024,f_s); title('Bode Diagrams of the used filter');
 
-spectrum = abs(fft(ecg - mean(ecg) ,4096));
+    spectrum = abs(fft(ecg - mean(ecg) ,4096));
 
-f = linspace(0,f_s,length(spectrum));
-figure(3); plot(f, spectrum);
+    f = linspace(0,f_s,length(spectrum));
+    
+    figure(3);
+    subplot(1,size(state_ecg,2),i); plot(f,spectrum); title(strcat(s,' -',' Filtered Spectrum'), 'Interpreter', 'none'); xlabel('Frequency [Hz]'); ylabel('|X(f)|');
 
 %% R-peaks detection
 
-[~,r_peaks_pt,~, ~, r_peaks_fp] = r_peaks_detection(ecg, f_s, 0, min_height(subject_number));
+    [~,r_peaks_pt,~, ~, r_peaks_fp] = r_peaks_detection(ecg, f_s, 0, min_height(subject_number));
 
-if subject_number == 4
-    ecg_flipped = flip(ecg);
-    [qrs_amp_raw,r_peaks_flip,delay, ~, ~] = r_peaks_detection(ecg_flipped,f_s,0,0);
-    r_peaks_pt = [r_peaks_pt(1:find(r_peaks_pt(:)==222200)-1), abs(r_peaks_flip(1:find(r_peaks_flip(:)==94141))-length(ecg))]; 
+    if subject_number == 4
+        ecg_flipped = flip(ecg);
+        [qrs_amp_raw,r_peaks_flip,delay, ~, ~] = r_peaks_detection(ecg_flipped,f_s,0,0);
+        r_peaks_pt = [r_peaks_pt(1:find(r_peaks_pt(:)==222200)-1), abs(r_peaks_flip(1:find(r_peaks_flip(:)==94141))-length(ecg))]; 
+    end
+    
+    if i>1
+        close 9;
+    end
+    
+    figure(4);
+    sb(1) = subplot(2,size(state_ecg,2),i); plot(t, ecg); hold on; plot((r_peaks_pt)/f_s, ecg(r_peaks_pt),'ok'); title(strcat(s,' -',' Pan-Tompkins Mod'), 'Interpreter', 'none'); ylabel('Amplitude [mV]');
+    sb(2) = subplot(2,size(state_ecg,2),i+size(state_ecg,2)); plot(t, ecg); hold on; plot((r_peaks_fp)/f_s, ecg(r_peaks_fp),'ok'); title(strcat(s,' -',' Find Peaks'), 'Interpreter', 'none'); ylabel('Amplitude [mV]');
+    xlabel('Time [s]'); 
+    linkaxes(sb,'x'); %to use the same axes for the subplots
+
+    %% Tachogram
+    
+    RRintervals = time_intervals(r_peaks_fp, f_s);
+
+    [x, y] = tachogram(RRintervals);
+    figure(5);
+    subplot(size(state_ecg,2),1,i); plot(x,y); title(strcat(s,' -',' ECG Tachogram')),xlabel('Beats'),ylabel('Time [s]');
+
+    %% Histogram
+
+    figure(6); % mettere a posto histogram: prendere il minimo e massimo generale dei segnali
+    subplot(1,size(state_ecg,2),i); histogram(y,ceil((max(y)-min(y))/(1/f_s))); title(strcat(s,' -',' ECG Histogram of RR peaks')),xlabel('Duration [s]'),ylabel('Occurrence');
+
+    %% Scattergram
+
+    figure(7);
+    subplot(1,size(state_ecg,2),i); plot(y(1:end-1),y(2:end),'.'); title(strcat(s,' -',' ECG Scattergram')),xlabel('(R-R)_{i}'),ylabel('(R-R)_{i+1}');
+
+    %% Time Domain Analysis
+    [avgHR, avgHRV, diff, RMSSD, SDNN] = time_domain_analysis(f_s, T, r_peaks_pt, RRintervals);
+
+    %% Frequency domain analysis
+    [LF2HF_welch, LF2HF_YW] = freq_domain_analysis(RRintervals, r_peaks_fp, f, f_s, size(state_ecg,2), i, s);
 end
-
-figure(4);
-sb(1) = subplot(2,1,1); plot(t, ecg); hold on; plot((r_peaks_pt)/f_s, ecg(r_peaks_pt),'ok'); title('Pan-Tompkins Mod', 'Interpreter', 'none'); ylabel('Amplitude [mV]');
-sb(2) = subplot(2,1,2); plot(t, ecg); hold on; plot((r_peaks_fp)/f_s, ecg(r_peaks_fp),'ok'); title('Find Peaks', 'Interpreter', 'none'); ylabel('Amplitude [mV]');
-xlabel('time [s]'); 
-linkaxes(sb,'x'); %to use the same axes for the subplots
-
-%% Tachogram
-RRintervals = time_intervals(r_peaks_fp, f_s);
-
-[x, y] = tachogram(RRintervals);
-figure(6);
-plot(x,y); title('ECG Tachogram'),xlabel('Beats'),ylabel('Time [s]');
-
-%% Histogram
-
-figure(7);
-histogram(y,ceil((max(y)-min(y))/(1/f_s))); title('ECG Histogram of RR peaks'),xlabel('Duration [s]'),ylabel('Occurrence');
-
-%% Scattergram
-
-figure(8);
-plot(y(1:end-1),y(2:end),'.'); title('ECG Scattergram'),xlabel('(R-R)_{i}'),ylabel('(R-R)_{i+1}');
-
-%% Time Domain Analysis
-[avgHR, avgHRV, diff, RMSSD, SDNN] = time_domain_analysis(f_s, T, r_peaks_pt, RRintervals);
-
-%% Frequency domain analysis
-[LF2HF_welch, LF2HF_YW] = freq_domain_analysis(RRintervals, r_peaks_fp, f, f_s);
