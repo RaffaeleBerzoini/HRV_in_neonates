231eeb43e6565441161d4f86e50eef0ccd3c0074
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2021-10-31 12:01:26 +0100

    Statistical analysis corrected

--- freq_stat_analysis.m
+++ freq_stat_analysis.m
@@ -38,10 +38,33 @@ function freq_stat_analysis(file_appendix)
     for i = 2:size(params, 2)-1
         quiet_vals = [mean(params{quiet, i}), std(params{quiet, i}), median(params{quiet, i}), iqr(params{quiet, i})];
         active_vals = [mean(params{active, i}), std(params{active, i}), median(params{active, i}), iqr(params{active, i})];
+       
+        warning('off') % to suppress the message "P is greater than the largest tabulated value, returning 0.5."
+        [hq_norm,~] = lillietest(params{quiet, i}); % h = 1 data are not normally distributed
+        [ha_norm,~] = lillietest(params{active, i});
+        fprintf('\nvar: %s, h_q_not_norm = %d, h_a_not_norm = %d', variables{i}, hq_norm, ha_norm);
+        if ~hq_norm && ~ha_norm %if both are normally distributed
+            [h, p] = ttest2(params{quiet, i}, params{active, i});% null hypothesis that the data in vectors x and y comes from independent random samples
+            if h == 0
+                fprintf('  not indipendent, tt\n');
+            else
+                fprintf(' indipendent, tt, p:%f\n', p);
+            end
+        else
+            [p, h] = ranksum(params{quiet, i},params{active, i});
+            if h == 1
+                fprintf('  indipendent, ww, p:%f\n', p);
+            else
+                fprintf(' not indipendent ww\n');
+            end
+        end
+        
+        
         [p, ~] = ranksum(params{quiet, i},params{active, i});
         writematrix([p], filename, Range=strcat('L', int2str(row))); %#ok<NBRAK> 
         writematrix(quiet_vals, filename, Range=strcat('B', int2str(row)));
         writematrix(active_vals, filename, Range=strcat('G', int2str(row)));
+             
         row = row + 1;
     end
 end
