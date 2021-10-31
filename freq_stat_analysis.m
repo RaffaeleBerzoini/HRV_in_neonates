5fe49dd4fdf31b20ad770a58db3f7454484c70b9
RaffaeleBerzoini <raffaele.berzoini@mail.polimi.it>
2021-10-31 17:02:33 +0100

    Statistical analysis completed

--- freq_stat_analysis.m
+++ freq_stat_analysis.m
@@ -26,45 +26,52 @@ function freq_stat_analysis(file_appendix)
     % write on file column's headers
 
     writecell({'QS'}, filename, Range='D1');
-    writecell({'AS'}, filename, Range='I1');
-    writecell({'Mean', 'SD', 'Median', 'IQR'}, filename, Range='B2');
-    writecell({'Mean', 'SD', 'Median', 'IQR'}, filename, Range='G2');
+    writecell({'AS'}, filename, Range='J1');
+    writecell({'Mean', 'SD', 'Median', 'IQR', strcat('N(',char(956),',',char(955),'^2',')')}, filename, Range='B2');
+    writecell({'Mean', 'SD', 'Median', 'IQR', strcat('N(',char(956),',',char(955),'^2',')')}, filename, Range='H2');
     writecell(variables(2:end)', filename, Range='A3:A14');
-    writecell({'MWW-p.Value'}, filename, Range='L2');
+    writecell({'p.Value'}, filename, Range='N2');
+    writecell({'MWW/ttest'}, filename, Range='O2');
+    
     
     % write on file all the statistical results obtained from the
     % parameters
+    
     row = 3;
     for i = 2:size(params, 2)-1
         quiet_vals = [mean(params{quiet, i}), std(params{quiet, i}), median(params{quiet, i}), iqr(params{quiet, i})];
         active_vals = [mean(params{active, i}), std(params{active, i}), median(params{active, i}), iqr(params{active, i})];
-       
+        
         warning('off') % to suppress the message "P is greater than the largest tabulated value, returning 0.5."
-        [hq_norm,~] = lillietest(params{quiet, i}); % h = 1 data are not normally distributed
-        [ha_norm,~] = lillietest(params{active, i});
-        fprintf('\nvar: %s, h_q_not_norm = %d, h_a_not_norm = %d', variables{i}, hq_norm, ha_norm);
-        if ~hq_norm && ~ha_norm %if both are normally distributed
-            [h, p] = ttest2(params{quiet, i}, params{active, i});% null hypothesis that the data in vectors x and y comes from independent random samples
-            if h == 0
-                fprintf('  not indipendent, tt\n');
+        if size(params, 1) >= 4 %tests non conductable if less than 4 observations are available
+            [hq_norm,~] = lillietest(params{quiet, i}); % h = 1 data are not normally distributed
+            [ha_norm,~] = lillietest(params{active, i});
+            
+            if ~hq_norm && ~ha_norm %if both are normally distributed
+                test_used = 'ttest';
+                [~, p] = ttest2(params{quiet, i}, params{active, i}); % h==1 different distribution
             else
-                fprintf(' indipendent, tt, p:%f\n', p);
+                test_used = 'MWW';
+                [p, ~] = ranksum(params{quiet, i},params{active, i}); % h==1 different distribution
             end
-        else
-            [p, h] = ranksum(params{quiet, i},params{active, i});
-            if h == 1
-                fprintf('  indipendent, ww, p:%f\n', p);
+            
+            if hq_norm
+                writematrix([0], filename, Range=strcat('F', int2str(row))) %#ok<*NBRAK> 
             else
-                fprintf(' not indipendent ww\n');
+                writematrix([1], filename, Range=strcat('F', int2str(row)))
             end
+            if ha_norm
+                writematrix([0], filename, Range=strcat('L', int2str(row)))
+            else
+                writematrix([1], filename, Range=strcat('L', int2str(row)))
+            end
+            
+            writematrix([test_used], filename, Range=strcat('O', int2str(row)));
+            writematrix([p], filename, Range=strcat('N', int2str(row))); 
         end
-        
-        
-        [p, ~] = ranksum(params{quiet, i},params{active, i});
-        writematrix([p], filename, Range=strcat('L', int2str(row))); %#ok<NBRAK> 
         writematrix(quiet_vals, filename, Range=strcat('B', int2str(row)));
-        writematrix(active_vals, filename, Range=strcat('G', int2str(row)));
-             
+        writematrix(active_vals, filename, Range=strcat('H', int2str(row)));
+    
         row = row + 1;
     end
 end
